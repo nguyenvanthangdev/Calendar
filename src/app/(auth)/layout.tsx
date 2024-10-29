d2c4427b2c42ca940d0c5e124ed38bd7479a4a9b
unknown <thangvan0969251032@gmail.com>
2024-10-29 23:44:42 +0700

    new sign-in and sign-up

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl">
+        <nav className="flex justify-between items-center">
+          <div className="flex gap-3 mx-2 my-5">
+            <Image src="/logo.svg" width={50} height={50} alt="Logo" />
+            <h3 className="text-4xl font-bold">Calendar</h3>
+          </div>
+          <div className="flex items-center gap-2">
+            <Button asChild className="text-xl" variant="default">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {isSignIn ? "Sign Up" : "Login"}
+              </Link>
+            </Button>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center py-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
